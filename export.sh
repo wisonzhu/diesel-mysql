b6652c77851bac449b6271fbef9b5bac5e7319b0
zhuxiaozhou <zhumyroot@gmail.com>
2024-07-22 19:02:26 +0800

    test diesel

---
+++ export.sh
@@ -0,0 +1,3 @@
+export LIBRARY_PATH="/opt/homebrew/opt/mysql-client/lib:$LIBRARY_PATH"
+export CPATH="/opt/homebrew/opt/mysql-client/include:$CPATH"
+export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
---
+++ src/bin/testnotice.rs
@@ -0,0 +1,44 @@
+// 按以下要求生成代码
+// post方法调用http://opengalaxy.bg.huohua.cn/api/v1/kube-notice/feishu/notice接口
+// json的数据格式如下:
+//  notice_token: 通知token
+//  notice_type: 通知类型，目前只支持text
+//  notice_data: 通知内容
+//  notice_at: 通知人列表，目前只支持手机号
+
+// 根据上面需求，生成代码如下
+use reqwest::Client;
+use serde_json::{from_str, Value};
+use std::collections::HashMap;
+
+#[tokio::main]
+
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+
+    let mut map = HashMap::new();
+    map.insert("notice_token", "token");
+    map.insert("notice_type", "text");
+    map.insert("notice_data", "data");
+    map.insert("notice_at", "at");
+
+    let res = client
+        .post("http://opengalaxy.bg.huohua.cn/api/v1/kube-notice/feishu/notice")
+        .json(&map)
+        .send()
+        .await?;
+
+    let text = res.text().await?;
+    let v: Value = from_str(&text)?;
+    println!("{:?}", v);
+
+    Ok(())
+}
+
+
+
+
+
+
+
+
